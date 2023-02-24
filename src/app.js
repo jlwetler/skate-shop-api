15bc19f5d8918162ef801a8a795d1aea65ef3765
jlwetler <joaolucasmw@gmail.com>
2023-02-24 14:28:21 -0300

    change import app method

--- src/app.js
+++ src/app.js
@@ -1,40 +1,40 @@
 import express from 'express';
 import cors from 'cors';
-import { categories, subCategories } from './controllers/categoriesController.js';
-import { brands, productBrand } from './controllers/brandsController.js';
-import { products, filterProducts } from './controllers/productsController.js'
-import { search } from './controllers/searchController.js';
-import { login } from './controllers/userController.js';
-import { signUp, signUpAddress } from './controllers/signUpController.js';
-import { getOrders, finishOrder } from './controllers/ordersController.js';
+import * as categoriesController from './controllers/categoriesController.js';
+import * as brandsController from './controllers/brandsController.js';
+import * as productsController from './controllers/productsController.js'
+import * as searchController from './controllers/searchController.js';
+import * as userController from './controllers/userController.js';
+import * as signUpController from './controllers/signUpController.js';
+import * as ordersController from './controllers/ordersController.js';
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/login', login); 
+app.post('/login', userController.login); 
 
-app.post('/sign-up', signUp); 
+app.post('/sign-up', signUpController.signUp); 
 
-app.post('/sign-up/address', signUpAddress); 
+app.post('/sign-up/address', signUpController.signUpAddress); 
 
-app.get('/categories', categories);
+app.get('/categories', categoriesController.categories);
 
-app.get('/subcategories/:category', subCategories);
+app.get('/subcategories/:category', categoriesController.subCategories);
 
-app.get('/brands', brands);
+app.get('/brands', brandsController.brands);
 
-app.get('/brands/:brandId', productBrand);
+app.get('/brands/:brandId', brandsController.productBrand);
 
-app.get('/products/:category', products);
+app.get('/products/:category', productsController.products);
 
-app.get('/products/filter/:category', filterProducts);
+app.get('/products/filter/:category', productsController.filterProducts);
 
-app.get('/search/:search', search);
+app.get('/search/:search', searchController.search);
 
-app.get('/get-orders', getOrders);
+app.get('/get-orders', ordersController.getOrders);
 
-app.post('/finish-order', finishOrder);
+app.post('/finish-order', ordersController.finishOrder);
 
 export default app;
