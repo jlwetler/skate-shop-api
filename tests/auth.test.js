1f212aae794aff210c3c78b103bee06a77480820
jlwetler <joaolucasmw@gmail.com>
2023-02-24 21:49:10 -0300

    auth test

---
+++ tests/auth.test.js
@@ -0,0 +1,17 @@
+import supertest from 'supertest';
+import app from '../src/app.js';
+
+describe("POST /sign-up", () => {
+    it("should respond with status 201 when there is no user with given email", async () => {
+        const body = {
+            name: 'Fulano',
+            lastName: 'DiTal',
+            email: 'fulano@email.com',
+            password: '123456'
+        }
+        
+        const response = await supertest(app).post("/sign-up").send(body);
+        
+        expect(response.status).toEqual(201);
+    });
+});
